c9f0fd9c16bfac62887f97043e229f15aafc100a
zkym <zkym4564@gmail.com>
2024-08-25 20:18:44 +0300

    fix

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 import MobileSidebar from './mobile-sidebar'
 import NavbarRoutes from '@/components/ui/navbar-routes'
 import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
 
 const Navbar = () => {
   const {userId} = auth();
+  if (!userId) return redirect("/");
   return (
     <div className='p-4 border-b h-full flex items-center bg-white shadow-sm'>
       <MobileSidebar />
